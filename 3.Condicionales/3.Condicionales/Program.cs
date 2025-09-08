4eb3f6ad7457bc5d8175c3a0eb703dc500ec873f
ghostman5226 <samiqt2004@gmail.com>
2025-09-08 14:39:20 -0500

    Condicionales

---
+++ 3.Condicionales/3.Condicionales/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita ingresar el nombre u el saldo de una persona, si el sueldo supera los 3000 pesos, mostrar el mensaje en pantalla con el nombre de la 
+             persona, indicando que debe abonar impuesto*/
+
+            //Condicional simple
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine(nombre);
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo > 3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuesto");
+            }
+        }
+    }
+}
